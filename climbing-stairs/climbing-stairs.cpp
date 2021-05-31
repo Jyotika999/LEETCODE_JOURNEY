524be1f9812c28adad72f72ca3000c106b393db6
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-05-31 11:14:59 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (77.78%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    int dp[50];
+    
+    
+    int solve(int n)
+    {
+    //     if(n==0)
+    //         return 1;
+        
+        if(n==1)
+            return 1;
+        
+        if(n==2)
+            return 2;
+        
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        return dp[n] = solve(n-1) + solve(n-2);
+    }
+    
+    int climbStairs(int n) {
+        
+        memset(dp, -1, sizeof(dp));
+        
+        return solve(n);
+        
+        
+    }
+};
