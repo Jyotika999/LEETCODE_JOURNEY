ae805f56508b37f57ff352abb3f7c600979979b7
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-06-30 00:17:28 +0530

    Time: 12 ms (97.27%), Space: 23.3 MB (67.36%) - LeetHub

--- max-area-of-island/max-area-of-island.cpp
+++ max-area-of-island/max-area-of-island.cpp
@@ -1,73 +1,44 @@
-// TIME COMPLEXITY : O(N*M)
-// SPACE : O(N*M)
-
-
-
 class Solution {
 public:
     
-    int n,m;
-    
-    
-    int vis[55][55];
-    int dx[4]={0, 1, 0, -1};
-    int dy[4]={1, 0, -1, 0};
-    int count=0;
-    bool invalid(int x, int y)
+    int ct=0;
+    void dfs(int i, int j, vector<vector<int>>&grid)
     {
-        return (x<0 || y<0 || x>=n || y>=m);
-    }
-    
-    void  solve(int x, int y, vector<vector<int>>& grid)
-    {
+        if(i<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==0)
+            return ;
         
-        vis[x][y]=1;
-        count++;
-        for(int i=0;i<4;i++)
-        {
-            int xx = x + dx[i];
-            int yy = y + dy[i];
-            
-            if(!invalid(xx, yy))
-            {
-                  if((vis[xx][yy]==0)  && (grid[xx][yy]==1))
-                    {
-                        solve(xx, yy, grid);
-                      //  count++;
-                    }
-            }
-          
-        }
+        grid[i][j]=0;
+        ct++;
         
-     
+        dfs(i, j+1, grid);
+        dfs(i, j-1, grid);
+        dfs(i+1, j, grid);
+        dfs(i-1, j, grid);
         
     }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         
-         n = grid.size();
-         m = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
         
-        memset(vis, 0, sizeof(vis));
-        int maxi =0;
+        int maxi=0;
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                    if(grid[i][j]==1)
-                    {
-                            solve(i, j, grid);
-                            maxi = max( maxi, count);
-                            memset(vis, 0, sizeof(vis));
-                            count=0;
-                    }
-                  
+                if(grid[i][j]==1)
+                {
+                    dfs(i, j, grid);
+                    maxi = max( maxi, ct);
+                }
+                ct=0;
+                
             }
         }
         
         return maxi;
-     
         
     }
 };
