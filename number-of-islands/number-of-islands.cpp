e6764890bbde0887df5151915f1121c3cd75e2b6
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-06-29 23:41:06 +0530

    Time: 8 ms (99.67%), Space: 9.4 MB (87.47%) - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    int n ,m;
-    int vis[305][305];
+    
+    // same as finding number of connnnected components in a graph 
     
     void dfs(int i, int j, vector<vector<char>>& grid)
     {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0')
+            return ;
         
-        if(i>=n or i<0 or j>=m or j<0 or grid[i][j]=='0' or vis[i][j]==1)
-            return;
         
-        vis[i][j]=1;
+        grid[i][j]='0';
         
         dfs(i+1, j, grid);
-        dfs(i, j+1, grid);
         dfs(i-1, j, grid);
+        dfs(i, j+1, grid);
         dfs(i, j-1, grid);
-        return;
+        
     }
     
     int numIslands(vector<vector<char>>& grid) {
         
-         n = grid.size();
-         m = grid[0].size();
-        
-        memset(vis, 0, sizeof(vis));
+        int n = grid.size();
+        int m = grid[0].size();
         int ct=0;
+        
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1' and vis[i][j]==0)
+                if(grid[i][j]=='1')
                 {
                     dfs(i, j, grid);
                     ct++;
@@ -38,5 +37,7 @@ public:
             }
         }
         return ct;
+        
+        
     }
 };
