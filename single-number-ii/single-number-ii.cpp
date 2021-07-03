632681571f551fdeabb32ec277bac8104006fa03
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-07-03 18:06:27 +0530

    Time: 4 ms (98.25%), Space: 9.6 MB (48.54%) - LeetHub

--- single-number-ii/single-number-ii.cpp
+++ single-number-ii/single-number-ii.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
       
-        
-        map<int,int>mp;
-        
-        int n = nums.size();
-        
-        for(int i=0;i<n;i++)
-        {
-            mp[ nums[i] ]++;
-        }
-        
-        for(auto it : mp)
-        {
-            int ff = it.first;
-            int ss = it.second;
-            if(ss==1)
-                return ff;
-        }
-        return -1;
+      int n =  nums.size();
+      int ans=0;
+        int ones=0;
+        int twos=0;
+      for(int i=0;i<n;i++)
+      {
+          int ele = nums[i];
+          
+          ones = (ones^ele) & ~(twos);
+          twos = (twos^ele) & ~(ones);
+      }
+        return ones;
     }
 };
+
+// First time number appear -> save it in "ones"
+
+// Second time -> clear "ones" but save it in "twos" for later check
+
+// Third time -> try to save in "ones" but value saved in "twos" clear it.
