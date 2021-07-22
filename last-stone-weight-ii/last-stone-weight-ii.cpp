040be7234b9ebcb0570264570d5fc3c86bdfd91d
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-07-22 11:09:49 +0530

    Time: 584 ms (5.15%), Space: 54 MB (5.00%) - LeetHub

--- last-stone-weight-ii/last-stone-weight-ii.cpp
+++ last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,25 +1,96 @@
 class Solution {
 public:
-    int lastStoneWeightII(vector<int>& stones) {
+    
+    int dp[105][100005];
+    
+//     int smash_stones( vector<int>&nums, int n , int sum)
+//     {
+//         if(sum==0)
+//         {
+//             return dp[n][sum]=1;
+//         }
+//         if(n==0)
+//         {
+//             return dp[n][sum]=0;
+//         }
+//         if(dp[n][sum]!=-1)
+//         {
+//             return dp[n][sum];
+//         }
         
-        int n =  stones.size();
+//         if(nums[n-1] <=  sum)
+//         {
+//             return dp[n][sum] = smash_stones(nums, n-1, sum) || smash_stones(nums, n-1, sum - nums[n-1]);
+//         }
+//         else
+//         {
+//             return dp[n][sum] = smash_stones(nums, n-1, sum);
+//         }
+        
+//     }
+    
+    
+    int lastStoneWeightII(vector<int>& stones) {
+    
+        int n = stones.size();
+        
         int sum=0;
+        
         for(int i=0;i<n;i++)
         {
             sum = sum + stones[i];
         }
-        int dp[n+1][sum+1];
-        memset(dp, 0, sizeof(dp));
         
-        for (int i = 1; i <= n; i++) {
-        for (int j = 0; j <= sum / 2; j++) {
-            if (j >= stones[i - 1]) {
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
-            } else {
-                dp[i][j] = dp[i - 1][j];
+        memset(dp, -1, sizeof(dp));
+        
+          for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                if(j==0)  // sum=0 
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+                if(i==0)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(stones[i-1]<= j)
+                {
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-stones[i-1]];
+                }
+                else
+                {
+                    dp[i][j]= dp[i-1][j];
+                }
             }
         }
-    }
-    return sum - 2 * dp[n][sum / 2];
+        
+      
+        
+        
+        int mini = INT_MAX;
+        
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=sum/2;j++)
+            {
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+        
+        
+        for(int i=0;i<=sum/2;i++)
+        {
+            if(dp[n][i] == 1)
+                mini =  min ( mini, abs(sum - 2*i));
+        }
+        
+        return mini;
+        
+        
     }
 };
