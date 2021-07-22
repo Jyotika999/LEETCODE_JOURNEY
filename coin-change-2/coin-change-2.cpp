f1364326289ca3e12fb1014f9ff00ab976b816a8
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-07-22 17:20:49 +0530

    Time: 56 ms (15.13%), Space: 20.6 MB (9.53%) - LeetHub

--- coin-change-2/coin-change-2.cpp
+++ coin-change-2/coin-change-2.cpp
@@ -1,36 +1,50 @@
 class Solution {
 public:
+    long long int dp[305][5005];
     
-    int dp[305][5005];
-    
-    int coinchange(int sum, vector<int>coins, int n)
+    long long int combine(int amount , vector<int>&coins, int n)
     {
-        if(sum==0)
-            return dp[n][sum]=1;
-        
-        if(n==0)
-            return dp[n][sum]=0;
-        
-        if(dp[n][sum]!=-1)
-            return dp[n][sum];
-        
-        if(coins[n-1]<=sum)
+        if(amount==0)
         {
-            return dp[n][sum] = coinchange(sum-coins[n-1] , coins, n)+ coinchange(sum, coins, n-1);   
+            return dp[n][amount] = 1;
+        }
+        if(n==0)
+        {
+            return dp[n][amount] = 0;
+        }
+        
+        if(dp[n][amount]!=-1)
+            return dp[n][amount];
+        
+        if(coins[n-1]<= amount)
+        {
+            return dp[n][amount] = combine(amount , coins, n-1) + combine(amount - coins[n-1] , coins, n);
         }
         else
         {
-            return dp[n][sum] = coinchange(sum, coins, n-1);
+            return dp[n][amount] = combine(amount, coins, n-1);
         }
     }
     
     int change(int amount, vector<int>& coins) {
         
-        
         int n = coins.size();
-        memset(dp, -1, sizeof(dp));
-        coinchange(amount, coins, n);
-        return dp[n][amount];
+        
+        // n = 0 , no coins , int_max 
+        // sum=0 , always possible , 1
+        // choice 
+        // if(cur coin <= amount)
+        //     include or exclude
+        // else
+        //     exclude always
+            
+    //    int dp[n+1][amount+1];
+        
+           memset(dp, -1, sizeof(dp));
+        
+            return combine(amount, coins, n);
+        
+        
         
     }
 };
