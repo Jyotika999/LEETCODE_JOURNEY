d71dd82dbe17cb356ef92b9639d9e2e77d46d9e1
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-07-25 15:20:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.99%) - LeetHub

---
+++ reverse-integer/reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        long long int num=0;
+        
+        while(x)
+        {
+            num = num*10 + x%10;
+            x/=10;
+        }
+        
+        if(num>INT_MAX || num<INT_MIN)
+            return 0;
+        
+        return num;
+        
+        
+    }
+};
