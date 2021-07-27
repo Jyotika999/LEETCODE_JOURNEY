e7950a7b891d784128916e4cfcbd4316637170bb
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-07-27 13:58:59 +0530

    Time: 8 ms (63.53%), Space: 13 MB (80.21%) - LeetHub

--- rotting-oranges/rotting-oranges.cpp
+++ rotting-oranges/rotting-oranges.cpp
@@ -1,90 +1,66 @@
 class Solution {
 public:
-    
-    bool valid(int i, int j, vector<vector<int>>&grid)
-    {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]==0)
-            return false;
-        
-        if(grid[i][j]==1)
-        grid[i][j]=2;
-        
-        return true;
-    }
+        int dx[4]={1, 0, 0, -1};
+        int dy[4]={0, 1, -1, 0};
     
     int orangesRotting(vector<vector<int>>& grid) {
         
-        int n = grid.size();
-        int m = grid[0].size();
-          vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
+        queue<pair<int,int>>q;
         
-        
-        // if the orange is rotten , then we will push the index of that orange index 
-        
-        queue<pair<int, int>>q;
-        
+        int m = grid.size();
+        int n = grid[0].size();
         int fresh=0;
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==2)
                 {
-                    q.push({i, j});
+                    q.push({i,j});
                 }
                 if(grid[i][j]==1)
                     fresh++;
             }
         }
         
-        int time=-1;
+        int time = -1;
+        
+        if(fresh==0)
+            return 0;
+        
         while(!q.empty())
         {
             int sz = q.size();
-         //   q.pop();
-            while(sz--)
+            
+            for(int i=1;i<=sz;i++)
             {
                 int x = q.front().first;
                 int y = q.front().second;
-                
-                
                 q.pop();
                 
-//                 if(valid(x+1, y, grid)) {q.push({x+1, y}); fresh--;}
-//                 if(valid(x-1, y, grid)) {q.push({x-1, y}); fresh--;}
-//                 if(valid(x, y+1, grid)) {q.push({x, y+1}); fresh--;}
-//                 if(valid(x, y-1, grid)) {q.push({x, y-1}); fresh--;}
- 
-                
-                for(int i=0;i<4;i++)
+                for(int j=0;j<4;j++)
                 {
-                    int r=x+dir[i];
-                    int c=y+dir[i+1];
-                    if(r>=0 && r<n && c>=0 && c<m &&grid[r][c]==1)
+                    int xx = x + dx[j];
+                    int yy = y + dy[j];
+                    if(xx>=0 and xx<m and yy>=0 and yy<n and  grid[xx][yy]==1)
                     {
-                        grid[r][c]=2;
-                        q.push({r,c});
-                        fresh--; // decrement by 1 foreach fresh orange that now is rotten
+                        grid[xx][yy]=2;
+                        q.push({xx, yy});
+                        fresh--;
                     }
-                    
                 }
-
+            
             }
             time++;
         }
         
         
-        
-        if(fresh>0)
+        if(fresh==0)
+            return time;
+        else
             return -1;
         
-        if(time==-1)
-            return 0;
         
-        return time;
     }
 };
