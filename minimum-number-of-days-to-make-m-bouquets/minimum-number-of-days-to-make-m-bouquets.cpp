14d576100fcacf894724ca792ddc8ef94113e540
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-07-28 13:55:27 +0530

    Time: 200 ms (27.91%), Space: 63.2 MB (64.10%) - LeetHub

--- minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,74 +1,67 @@
-// ARRAY OF SIZE N 
-// COMPLEXITY : O(NLOGM)
-// MINIMUM NIKALNA HAI KUCH 
-// PREDICATE FUNCTION PATTERN WILL BE OF THE FORMAT : ......FFFFFFFTTTTTTTTT.......
-
 class Solution {
 public:
-    
-    
-    
-    bool func(int mid, vector<int>&ar, int m , int k , int n)
-    {
-        int boq=0;
-        int flowers=0;
-        
-        for(int i=0;i<n;i++)
+   int  make_boques( int   cur_day, vector<int>&bloomday,  int k )
         {
-            if(ar[i]<=mid)
+            int count_boq =0;
+            int flowers = bloomday.size();
+            int consecutive_flower = 0;
+            for( int cur_flower =0 ; cur_flower< flowers ; cur_flower++)
             {
-                flowers++;
+                if( bloomday[cur_flower] <= cur_day)
+                {
+                    consecutive_flower++;
+                   
+                }
+                else
+                {
+                   
+                    consecutive_flower =0;
+                }	
+                
+                         if(consecutive_flower == k)
+                            {	
+                                count_boq++;
+                                consecutive_flower = 0;
+                            }
+
+                
             }
-            else
-            {
-                flowers=0;
-            }
+       
+       
             
-            if(flowers==k)
-            {
-                boq++;
-                flowers=0;
-            }
+            return count_boq; 
         }
-        
-        if(boq>=m)
-            return true;
-        else
-            return false;
-    }
     
-    int minDays(vector<int>& ar, int m, int k) {
+    
+    int minDays(vector<int>& bloomday, int m, int k) {
         
-        int low = INT_MAX;
-        int high = INT_MIN;
-        int n = ar.size();
-        for(int i=0;i<n;i++)
-        {
-            low = min(low, ar[i]);
-            high = max(high, ar[i]);
-        }
-       // cout<<low<<" "<<high<<"\n";
-        int ans=-1;
-        
-        while(low<=high)
-        {
-            int mid = (low)+(high-low)/2;
-            // FFFFFFFFFFFFFFFFFFFFTTTTTTTTTTTTTTTTTTT (my ans will be first True)
-            
-            if(func(mid, ar, m, k, n)==true)
+   	     int flowers = bloomday.size();
+   	    int maxday = 0;
+
+	    for( int cur_flower =0 ; cur_flower< flowers ; cur_flower++)
+        	  	        maxday = max( maxday , bloomday[cur_flower]);  
+
+
+            int start_day = 0;
+            int end_day = maxday ;
+            int min_days_required = -1;
+            while( start_day <= end_day)
             {
-                ans = mid;
-                high  = mid-1;
+                int mid_day = (start_day + end_day)/2;
+                cout<<mid_day<<" "<<make_boques(mid_day, bloomday, k)<<"\n";
+                if( make_boques( mid_day, bloomday, k ) < m)
+                {
+                    start_day = mid_day + 1;
+                }
+                else
+                {
+                    min_days_required = mid_day;
+                    end_day = mid_day -1;
+                }
             }
-            else
-            {
-                low = mid+1;
-            }
-        }
-        
-        return ans;
-        
-        
-        
+	
+           return min_days_required;  
     }
 };
+
+
