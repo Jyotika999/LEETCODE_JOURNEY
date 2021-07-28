1e0477299e1e20bdc818f1fd250ddd006e21b66c
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-07-28 16:03:21 +0530

    Time: 72 ms (26.38%), Space: 25.9 MB (94.36%) - LeetHub

--- capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,83 +1,62 @@
-	// ARRAY OF SIZE N
-	// COMPLEXITY : O(NLOGM)
-	// MINIMUM NIKALNA HAI KUCH
-	// PREDICATE FUNCTION PATTERN WILL BE OF THE FORMAT : ......FFFFFFFTTTTTTTTT.......
-	
+
+
 
 
 class Solution {
 public:
-    
-    
-//     bool valid(int dailywt, vector<int>&weights, int days, int n)
-//     {
-//         int reqd_days=1;
-//         int prev=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             if(prev+weights[i] > dailywt)
-//             {
-//                 reqd_days++;
-//                 prev= 0;
-//             }
-//             prev+= weights[i];
-//         }
-        
-//         cout<<dailywt<<" "<<reqd_days<<" "<<days<<"\n";
-//         if(reqd_days <= days)
-//             return true;
-//         else
-//             return false;
-        
-//     }
-    bool isvalid(vector<int> &weights,int days,int curr_ans){
-        int days_now=1;
-        int curr_wt=0;
-        for(int i=0;i<weights.size();i++){
-            if(weights[i]>curr_ans)return false;
-            
-            if(curr_wt+weights[i]>curr_ans){
-                curr_wt=weights[i];
-                days_now++;
-                if(days_now>days)return false;
-            }
-            else{
-                curr_wt+=weights[i];
-            }
-        }
-        return true;
-    }
-    
-    int shipWithinDays(vector<int>& weights, int days) {
-        
-        int n = weights.size();
-        
-        int high=0;
-        for(int i=0;i<n;i++)
-            high+= weights[i];
-        
-        int low=0;
-     //   int high= 50000;
-        int ans=0;
-        
-        while(low<=high)
-        {
-               int mid = (low)+(high-low)/2;
-                if(isvalid( weights, days, mid)==true)
+        bool  delivarable_intime( int   cur_wt, vector<int>&weights,  int  required_days )
+    {
+            int sum_wts =0;
+            int days=1;
+            for( int wt =0 ; wt< weights.size() ; wt++)
+            {
+                if( weights[ wt] > cur_wt )
+                    return false;
+                if( sum_wts + weights[wt ] <= cur_wt)
                 {
-                    ans  = mid;
-                    high = mid-1;
+                    sum_wts = sum_wts + weights[wt];
                 }
                 else
                 {
-                    low = mid+1;
+                    days++;
+                    if( weights[ wt] > cur_wt )
+                    return false;
+
+                    sum_wts =  weights[ wt];
                 }
-        }
+            }
+
+        if( days<= required_days) return true;
+        else return false;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
         
-        return ans;
-        
-        
-        
-        
+	int total_weights = weights.size();
+	int sum_weights = 0;
+	for( int cur_weight = 0; cur_weight < total_weights ; cur_weight++)
+	{
+		sum_weights = sum_weights + weights[ cur_weight ] ;
+	}
+
+	int low_wt = 0;
+	int high_wt = sum_weights;
+	int min_wt =0;
+
+	while ( low_wt <= high_wt) 
+	{
+		int mid_wt = (low_wt + high_wt )/2;
+		if( delivarable_intime( mid_wt, weights, days ))
+		{
+			min_wt = mid_wt ; 
+			high_wt = mid_wt -1;
+		}
+		else
+		{
+			low_wt = mid_wt + 1;
+		}
+
+	}
+	return min_wt;
     }
 };
