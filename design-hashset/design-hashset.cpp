c6f2fdd91b987f97f86e991e3f86efcab7ab17db
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2021-12-13 09:49:31 +0530

    Time: 92 ms (62.76%), Space: 179.8 MB (18.35%) - LeetHub

---
+++ design-hashset/design-hashset.cpp
@@ -0,0 +1,29 @@
+class MyHashSet {
+public:
+    
+    vector<int>m;
+    MyHashSet() {
+        int sz=1e6+5;
+        m.resize(sz);
+    }
+    
+    void add(int key) {
+        m[key]=1;
+    }
+    
+    void remove(int key) {
+        m[key]=0;
+    }
+    
+    bool contains(int key) {
+        return m[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
