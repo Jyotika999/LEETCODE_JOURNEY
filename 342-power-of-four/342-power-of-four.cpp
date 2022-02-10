3a14dabed9785cd12bb3edf9c48c0f19790f5348
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-02-10 22:50:12 +0530

    Time: 4 ms (36.24%), Space: 6 MB (25.37%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<0)
+            return false;
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        if(n<=3)
+            return false;
+            
+        
+        if(n%4!=0)
+            return false;
+        
+        return isPowerOfFour(n/4);
+        
+    }
+};
