b7efef2d4304daaecd65e9e21a579973bb820cac
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-02-15 11:47:09 +0530

    Time: 4 ms (57.49%), Space: 7.6 MB (55.55%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL)
+        {
+            return head;
+        }
+        
+        
+        // aage ki bachee huyi sari linkedlist ko reverse krke leke aayo
+        ListNode * aage = swapPairs(head->next->next);
+        
+        // abhi k 2 nodes ko hum reverse kr denge 
+        ListNode * temp1 = head->next;
+        ListNode * temp = head;
+        temp1->next = temp;
+        temp->next = aage;
+        // head->next = head;
+        // head= temp;
+       //head->next = aage;
+        aage = temp1;
+        return aage;
+        
+    }
+};
