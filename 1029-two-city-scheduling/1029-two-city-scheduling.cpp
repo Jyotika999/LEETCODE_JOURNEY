a99fb7661c7b09fec4fee657634cabc0a11bfbd9
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-03-25 08:49:09 +0530

    Time: 31 ms (5.79%), Space: 10.2 MB (7.87%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,3 +1,5 @@
+/*Those who find it hard understanding the significance of the difference :-For every person we have to select either city A or City B for sure.But the case is we can only send half of the people to A and remaining to B. Example we have 100 people we have to send 50 to A and 50 to B. once we sent 50 people to A we have no other option but to send remaining to B, and pay the price of B. So the 50 A we selected should be such that the price of B corresponding to those A should be as higher than A as possible(ie, B-A should be as high as possible) so (Avoiding that B is most beneficial for us ). Hence we are taking difference.*/
+
 class Solution {
 public:
     
