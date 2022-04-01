58639238f4da86eecd92975a70263c88f6492be7
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-04-01 11:01:34 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (94.94%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    
     int dp[1005];
-    int solve(vector<int>&nums, int ind, int n)
+    
+    int solve(int i, vector<int>&nums)
     {
-        if(ind>=n)
+        if(i>=nums.size())
             return 0;
         
-        if(dp[ind]!=-1)
-            return dp[ind];
+        if(dp[i]!=-1)
+            return dp[i];
         
-        // include
-        int include = nums[ind] + solve(nums, ind+2, n);
-        // exclude
-        int exclude = solve(nums, ind+1, n);
-        
-        return dp[ind]= max(include, exclude);
-        
-        
+        return dp[i]=max(nums[i]+solve(i+2, nums), solve(i+1, nums));
     }
     int rob(vector<int>& nums) {
         
+        memset(dp, -1, sizeof(dp));
         int n = nums.size();
         
-        memset(dp, -1, sizeof(dp));
-        
-        solve(nums, 0 , n);
-        int maxi = INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-           maxi  = max(maxi, dp[i]);
-        }
-        return maxi;
+        return solve(0, nums);
         
     }
 };
+
+// take care , why greedy wont work
+// take the test case nums=[2, 1, 1, 2]
+// here the max ans will be 4 , but if you choose greedily then it will be 3 
+// hence greedy fails
+// becoz its not compulsory for every other adjacent to choose
+// you always have a choice 
+// either to include current element 
+// or exclude current element
+// you have decision to make
+// you have to maximise the totalcost 
