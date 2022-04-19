4bdf3f1961084d54d26d667ccb15edcc786b09ba
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-04-19 16:05:21 +0530

    Time: 75 ms (41.72%), Space: 15.9 MB (83.47%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,63 +1,59 @@
 class Solution {
 public:
     
-    
-//     int dx[3]={-1,  0};
-//     int dy[3]={0, -1};
+    int dx[4]={1, -1, 0, 0};
+    int dy[4]={0, 0, 1, -1};
     int dp[505][505];
-
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
-
-int max1(int a, int b, int c, int d)
-{
-    return max(max(a,b), max(c,d));
-}
-
-int path(int i, int j, int n, int m, vector<vector<int>>&matrix)
-{
-    if(i<0 || j<0 || i>=n || j>=m)
-    return 0;
-
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-
-    int maxi=0;
-    for(int k=0;k<4;k++)
+    
+    int solve(int i, int j, vector<vector<int>>&matrix, int &n, int &m)
     {
-        int xx = i + dx[k];
-        int yy = j + dy[k];
-
-        if(xx>=0 and xx<n and yy>=0 and yy<m and (matrix[i][j] < matrix[xx][yy]))
+        if( i<0 || j<0 || i>=n || j>=m )
+            return 0;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int maxi=1;
+        for(int k=0;k<4;k++)
         {
-            maxi = max(maxi, path(xx, yy, n, m, matrix));
-        }
-    }
-return dp[i][j] = maxi+1;
-
-  
-
-
-}
-
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-         
-    int n = matrix.size();
-    int m = matrix[0].size();
-    memset(dp, -1, sizeof(dp));
-int ans=0;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
-        {
-            if(dp[i][j]==-1)
+            int xx = i + dx[k];
+            int yy = j + dy[k];
+            
+            if(xx>=0 and yy>=0 and xx<n and yy<m and (matrix[xx][yy]>matrix[i][j]))
             {
-              ans = max(ans,   path(i, j, n, m, matrix));
+                // if(dp[xx][yy]!=-1)
+                //     maxi = max(maxi, dp[xx][yy]);
+                // else
+                maxi = max(maxi, 1+solve(xx, yy, matrix, n, m));
             }
         }
-    }
+        
+        return dp[i][j]=maxi;
 
-return ans;
-      
+    }
+    
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        // vector<vector<int>>dp(n, vector<int>(m, -1));
+        memset(dp, -1, sizeof(dp));
+        // solve(0, 0, matrix, n, m, dp);
+        
+        int maxi=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(dp[i][j]==-1)
+                maxi = max(maxi, solve(i, j, matrix, n, m));
+            }
+        }
+        return maxi;
     }
 };
