a59330d23ff22cbc75bbc06230616c31d853fb30
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-04-30 23:55:52 +0530

    Time: 109 ms (5.05%), Space: 27.4 MB (5.01%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,39 +1,95 @@
+class Node{
+    public:
+    char data ;
+    map<char, Node*>mp;
+    bool terminal ;
+    Node(char d)
+    {
+        data = d;
+        terminal = false;
+    }
+};
+
+class Trie{
+    public:
+    Node * root ;
+    Trie()
+    {
+        root = new Node('\0');
+    }
+    void insert(string t)
+    {
+        Node * temp = root;
+        int len = t.length();
+        for(int i=0;i<len;i++)
+        {
+            char cur = t[i];
+            if(temp->mp.count(cur)==0)
+            {
+                temp->mp[cur]= new Node(cur);
+            }
+            temp = temp->mp[cur];
+        }
+        temp->terminal = true;
+    }
+    
+    
+    bool search(string s)
+    {
+        Node * temp = root;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s[i];
+            if(temp->mp[ch])
+            {
+                temp = temp->mp[ch];
+            }
+            else
+                return false;
+        }
+        return temp->terminal;
+    }
+   
+};
+
+
 class Solution {
 public:
-     map<string, int>mp;
+    
+     bool check(string s,  Trie &t, map<string,bool>&mp )
+    {
         
-    bool wordBreak(string s, vector<string>& words) {
         
         int n = s.length();
-        int n1 = words.size();
+        if(n==0)
+            return true;
+         
+         if(mp.count(s))
+             return mp[s];
         
-        for(int i=0;i<n1;i++)
+        for(int k=0;k<=n;k++)
         {
-            mp[words[i]]++;
-        }
-        
-        vector<int>dp(n+1, 0);
-        
-        dp[n]=1;
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=i;j<n;j++)
+            string str1 = s.substr(0, k);
+            string str2 = s.substr(k, n);
+            if(t.search(str1) and check(str2, t, mp))
             {
-                string str1 = string(&s[i], &s[j+1]);
-                // string str2 = s.substr(j+1, n);
-                if(mp[str1] and dp[j+1]==1)
-                {
-                    dp[i]=1;
-                    break;
-                }
+                return mp[s]=true;
             }
         }
+        return mp[s]=false;
+    }
+    bool wordBreak(string s, vector<string>& word) {
         
+        map<string,bool>mp;
+        Trie t ;
+        int n = word.size();
         for(int i=0;i<n;i++)
-            cout<<dp[i]<<"\n";
-        return dp[0];
+        {
+            t.insert(word[i]);
+        }
+        // return true;
         
+        return check(s, t, mp);
         
         
     }
