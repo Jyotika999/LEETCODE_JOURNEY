01e982a665661d55773294ddf6008af81dc96b18
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-05-19 08:20:37 +0530

    Time: 254 ms (12.15%), Space: 16.3 MB (41.90%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -3,17 +3,16 @@ public:
     
     int dx[4]={1, -1, 0, 0};
     int dy[4]={0, 0, 1, -1};
-    int dp[505][505];
     
-    int solve(int i, int j, vector<vector<int>>&matrix, int &n, int &m)
+    int solve(int i, int j, int n, int m, vector<vector<int>>&matrix, vector<vector<int>>&dp)
     {
-        if( i<0 || j<0 || i>=n || j>=m )
+        if(i<0 || j<0 || i>=n || j>=m)
             return 0;
         
         if(dp[i][j]!=-1)
             return dp[i][j];
         
-        int maxi=1;
+        dp[i][j]=1;
         for(int k=0;k<4;k++)
         {
             int xx = i + dx[k];
@@ -21,39 +20,39 @@ public:
             
             if(xx>=0 and yy>=0 and xx<n and yy<m and (matrix[xx][yy]>matrix[i][j]))
             {
-                // if(dp[xx][yy]!=-1)
-                //     maxi = max(maxi, dp[xx][yy]);
-                // else
-                maxi = max(maxi, 1+solve(xx, yy, matrix, n, m));
+                    dp[i][j] = max(dp[i][j], 1+ solve(xx, yy, n, m, matrix, dp));              
             }
         }
-        
-        return dp[i][j]=maxi;
-
+        return dp[i][j];
     }
-    
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
         
         int n = matrix.size();
         int m = matrix[0].size();
         
-        // vector<vector<int>>dp(n, vector<int>(m, -1));
-        memset(dp, -1, sizeof(dp));
-        // solve(0, 0, matrix, n, m, dp);
+        vector<vector<int>>dp(n+5, vector<int>(m+5, -1));
         
-        int maxi=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(dp[i][j]==-1)
-                maxi = max(maxi, solve(i, j, matrix, n, m));
+                    dp[i][j] = max(dp[i][j], solve(i, j, n, m, matrix, dp));
             }
         }
+        
+        int maxi = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cout<<dp[i][j]<<" ";
+                maxi = max(maxi, dp[i][j]);
+            }
+            cout<<"\n";
+        }
+        
         return maxi;
+        
     }
 };
