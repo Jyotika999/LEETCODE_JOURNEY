79fa073ecd0cf2eafea09add1cbefdca3bf62924
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-06-06 07:49:44 +0530

    Time: 17 ms (11.10%), Space: 8.2 MB (97.35%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -2,15 +2,40 @@ class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         
-      //  https://leetcode.com/problems/sum-of-all-odd-length-subarrays/discuss/854184/JavaC%2B%2BPython-O(N)-Time-O(1)-Space
-        
         int n = arr.size();
         int ans=0;
         for(int i=0;i<n;i++)
         {
-           ans+= (((n-i)*(i+1)+1)/2)*arr[i];
+            
+            for(int j=i;j<n;j++)
+            {
+                
+                if((j-i+1)&1)
+                {
+                    int sum=0;
+                    for(int k=i;k<=j;k++)
+                    {
+                        sum+=arr[k];
+                    }
+                    ans+=sum;
+                }
+            }
         }
         
         return ans;
     }
 };
+
+// i, j
+// i=0, j=0
+// len of subarray between i and j = j-i+1
+// len = odd
+// sum+= currentsum
+// len = even
+// i++
+// len = oddd
+
+// case, len=even
+// j++
+// len = odd
+// sum sath m calculate kro 
