a70dea856bf05e33599e83d50c1730cf53323e43
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-06-13 22:45:35 +0530

    Time: 15 ms (14.49%), Space: 8.7 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,53 +1,42 @@
 class Solution {
 public:
     
-    int dp[500][500];
+    int dp[205][205];
     
-    int solve(int i, int j, vector<vector<int>>&triangle, int n, int m)
+    int solve(int i, int j, vector<vector<int>>&triangle, int &n)
     {
+        if(i>=n)
+            return 0;
         
-        if(i<0 || j<0 || j>=triangle[i].size())
-            return 1e9;
+        if(j>=triangle[i].size())
+            return 0;
         
         if(dp[i][j]!=-1)
             return dp[i][j];
+        // either choose i th index, or choose i+1 th index
         
-        if(i==0 and j==0)
-            return dp[i][j]=triangle[i][j];
-                
-
-       
-        // i th row , jth col 
-        // jth col , i-1 row,
-        // j-1th col, i-1 row 
+        int t1 = solve(i+1, j, triangle, n);
+        int t2 = solve(i+1, j+1, triangle, n);
         
-        int same_col = solve(i-1, j, triangle, n, m);
-        int prev_col = solve(i-1, j-1, triangle, n, m);
-        
-        return dp[i][j]=min(same_col, prev_col)+triangle[i][j];
+        return dp[i][j]= min(t1, t2)+triangle[i][j];
     }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int n = triangle.size();
-        int m = triangle[n-1].size();
-        int ans = INT_MAX;
+        
+        // if you are on index i , then we can move to ith row or (i+1)th row
+        
         memset(dp, -1, sizeof(dp));
         
-        for(int i=0;i<m;i++)
-        {
-            // triangle[n-1][i];
-            ans = min(ans, solve(n-1, i, triangle, n, m));
-        }
-        return ans;
+        // minimum path sum from top to bottom 
+        return solve(0, 0, triangle, n);
+        
+        
+        
+        
+        
     }
 };
-
-// 2
-// 3 4
-// 6 5 7
-// 4 1 8 3
-
-    
-
 
 
