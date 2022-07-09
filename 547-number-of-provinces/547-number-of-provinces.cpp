56ecd1e5d57a780f4e3adcdcbcb3ec13c480bed4
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2022-07-09 14:31:28 +0530

    Time: 23 ms (93.30%), Space: 13.7 MB (63.26%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,62 +1,76 @@
 class Solution {
 public:
-    int parent[205];
-    int sz[205];
     
-    int find_parent(int i)
+    void makeset(vector<int>&parent, vector<int>&rank, int n)
     {
-        if(parent[i]==i)
-            return i;
-        
-        return parent[i]=find_parent(parent[i]);
-    }
-    void unionn(int i,int j)
-    {
-        i = find_parent(i);
-        j = find_parent(j);
-        
-        if(i!=j)
-        {
-            if(sz[i]<sz[j])
-            {
-                sz[j]+=sz[i];
-                parent[i]=j;
-            }
-            else
-            {
-                sz[i]+=sz[j];
-                parent[j]=i;
-            }
-        }
-    }
-    int findCircleNum(vector<vector<int>>&grid) {
-        
-        int n = grid.size();
         for(int i=0;i<n;i++)
         {
             parent[i]=i;
-            sz[i]=1;
+            rank[i]=0;
         }
+    }
+    
+    int findparent(int i, vector<int>&parent)
+    {
+        if(i==parent[i])
+            return i;
+        return parent[i]= findparent(parent[i], parent);
+    }
+    void unionn(int i, int j, vector<int>&parent, vector<int>&rank)
+    {
+        i = findparent(i, parent);
+        j = findparent(j, parent);
+        
+        if(i!=j)
+        {
+            if(rank[i]<rank[j])
+            {
+                parent[i]=j;
+            }
+            else if(rank[i]>rank[j])
+            {
+                parent[j]=i;
+            }
+            else
+            {
+                parent[j]=i;
+                rank[i]++;
+            }
+        }
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        
+        int n = isConnected.size();
+        
+        // vector<int>graph[n+1];
+        vector<int>parent(n, 0);
+        vector<int>rank(n, 0);
+        
+        makeset(parent, rank, n);
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j]==1)
-                {
-                    unionn(i, j);
-                }
+                // graph[i].push_back(j);
+                // graph[j].push_back(i);
+                if(isConnected[i][j]==1)
+                unionn(i, j, parent, rank);
             }
         }
         
-        int ct=0;
+        int provinces=0;
+        
         for(int i=0;i<n;i++)
         {
             if(parent[i]==i)
             {
-                ct++;
+                provinces++;
             }
         }
-        return ct;
+        
+        return provinces;
     }
 };
+
